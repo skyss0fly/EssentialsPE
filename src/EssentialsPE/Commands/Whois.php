4707991c48ad4b1205f1f4929c9ccc64b33acf14
ZPlayzMCPE <jameshayward3800@gmail.com>
2017-09-18 12:44:31 +0100

    Update Whois.php

--- src/EssentialsPE/Commands/Whois.php
+++ src/EssentialsPE/Commands/Whois.php
@@ -21,7 +21,7 @@ class Whois extends BaseCommand{
      * @param array $args
      * @return bool
      */
-    public function execute(CommandSender $sender, string $alias, array $args): bool{
+    public function execute(CommandSender $sender, $alias, array $args) {
         if(!$this->testPermission($sender)){
             return false;
         }
