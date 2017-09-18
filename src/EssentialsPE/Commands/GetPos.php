45c32941b7e1b92d509ca894f74087f1b0d3117b
ZPlayzMCPE <jameshayward3800@gmail.com>
2017-09-18 11:45:06 +0100

    Update GetPos.php

--- src/EssentialsPE/Commands/GetPos.php
+++ src/EssentialsPE/Commands/GetPos.php
@@ -22,7 +22,7 @@ class GetPos extends BaseCommand{
      * @param array $args
      * @return bool
      */
-    public function execute(CommandSender $sender, string $alias, array $args): bool{
+    public function execute(CommandSender $sender, $alias, array $args) {
         if(!$this->testPermission($sender)){
             return false;
         }
