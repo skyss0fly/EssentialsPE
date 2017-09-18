61c3c326add9848923d3bc0a5504243f46cab565
ZPlayzMCPE <jameshayward3800@gmail.com>
2017-09-18 11:48:35 +0100

    Update Hat.php

--- src/EssentialsPE/Commands/Hat.php
+++ src/EssentialsPE/Commands/Hat.php
@@ -24,7 +24,7 @@ class Hat extends BaseCommand{
 	 *
 	 * @return bool
 	 */
-    public function execute(CommandSender $sender, string $alias, array $args): bool{
+    public function execute(CommandSender $sender, $alias, array $args) {
         if(!$this->testPermission($sender)){
             return false;
         }
